c3b68e86b141d371b36b6414762b77a516d55239
Alex Dorsett <adorsett20@gmail.com>
2019-10-30 12:49:47 -0700

    Updated mu_producer for Zgamma_channelIslands production.

--- inc/mu_producer.hpp
+++ inc/mu_producer.hpp
@@ -13,10 +13,12 @@ public:
 
   const float SignalMuonPtCut  = 20.0;
   const float VetoMuonPtCut    = 10.0;
-  const float MuonEtaCut         = 2.4;
-  const float MuonMiniIsoCut     = 0.2;
+  const float ZgMuonPtCut      =  5.0;
+  const float MuonEtaCut        = 2.4;
+  const float MuonMiniIsoCut    = 0.2;
+  const float MuonRelIsoCut     = 0.35;
 
-  std::vector<int> WriteMuons(nano_tree &nano, pico_tree &pico, std::vector<int> &jet_islep_nano_idx);
+  std::vector<int> WriteMuons(nano_tree &nano, pico_tree &pico, std::vector<int> &jet_islep_nano_idx, bool isZgamma);
 
 private:
   int year;
--- src/mu_producer.cpp
+++ src/mu_producer.cpp
@@ -11,29 +11,43 @@ MuonProducer::MuonProducer(int year_){
 MuonProducer::~MuonProducer(){
 }
 
-vector<int> MuonProducer::WriteMuons(nano_tree &nano, pico_tree &pico, vector<int> &jet_islep_nano_idx){
+vector<int> MuonProducer::WriteMuons(nano_tree &nano, pico_tree &pico, vector<int> &jet_islep_nano_idx, bool isZgamma){
 
   vector<int> sig_mu_nano_idx;
   pico.out_nmu() = 0; pico.out_nvmu() = 0;
   for(int imu(0); imu<nano.nMuon(); ++imu){
-    if (!nano.Muon_mediumId()[imu]) continue;
-    if (nano.Muon_pt()[imu] <= VetoMuonPtCut) continue;
-    if (fabs(nano.Muon_eta()[imu]) > MuonEtaCut) continue;
-
-    bool isSig = false;
-    if (nano.Muon_pt()[imu] > SignalMuonPtCut &&
-      nano.Muon_miniPFRelIso_all()[imu] < MuonMiniIsoCut)
-      isSig = true;
-
-    pico.out_mu_pt().push_back(nano.Muon_pt()[imu]);
-    pico.out_mu_eta().push_back(nano.Muon_eta()[imu]);
+    float pt = nano.Muon_pt()[imu];
+    float eta = nano.Muon_eta()[imu];
+    bool isSignal = false;
+    if(isZgamma) { // For Zgamma productions
+      if (pt <= ZgMuonPtCut) continue;
+      if (fabs(eta) > MuonEtaCut) continue;
+      if (fabs(nano.Muon_dz()[imu])>1.0)  continue;
+      if (fabs(nano.Muon_dxy()[imu])>0.5) continue; 
+      if ((nano.Muon_looseId()[imu] || (pt > 200 && nano.Muon_highPtId()[imu])) && 
+           nano.Muon_pfRelIso03_all()[imu] < MuonRelIsoCut &&
+           nano.Muon_sip3d()[imu] < 4)
+        isSignal = true;
+      pico.out_mu_sip3d().push_back(nano.Muon_sip3d()[imu]);
+    }
+    else {
+      if (!nano.Muon_mediumId()[imu]) continue;
+      if (pt <= VetoMuonPtCut) continue;
+      if (fabs(eta) > MuonEtaCut) continue;
+      if (pt > SignalMuonPtCut &&
+        nano.Muon_miniPFRelIso_all()[imu] < MuonMiniIsoCut)
+        isSignal = true;
+    }
+    pico.out_mu_pt().push_back(pt);
+    pico.out_mu_eta().push_back(eta);
     pico.out_mu_phi().push_back(nano.Muon_phi()[imu]);
     pico.out_mu_miniso().push_back(nano.Muon_miniPFRelIso_all()[imu]);
     pico.out_mu_reliso().push_back(nano.Muon_pfRelIso03_all()[imu]);
     pico.out_mu_dz().push_back(nano.Muon_dz()[imu]);
     pico.out_mu_dxy().push_back(nano.Muon_dxy()[imu]);
     pico.out_mu_ip3d().push_back(nano.Muon_ip3d()[imu]);
-    pico.out_mu_sig().push_back(isSig);
+    pico.out_mu_id().push_back(nano.Muon_looseId()[imu]);
+    pico.out_mu_sig().push_back(isSignal);
     pico.out_mu_charge().push_back(nano.Muon_charge()[imu]);
     pico.out_mu_pflavor().push_back(nano.Muon_genPartFlav()[imu]);
 
@@ -41,7 +55,7 @@ vector<int> MuonProducer::WriteMuons(nano_tree &nano, pico_tree &pico, vector<in
       pico.out_nvmu()++;
       pico.out_nvlep()++;
     }
-    if (isSig) {
+    if (isSignal) {
       pico.out_nmu()++;
       pico.out_nlep()++;
       sig_mu_nano_idx.push_back(imu);
