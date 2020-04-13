c3f5da585f855161efee8444ede01c794f6f770c
Ana Ovcharova <akovcharova@gmail.com>
2020-04-13 15:44:27 -0400

    Exclude tracks that are not PFCands or from lost track

--- src/tk_producer.cpp
+++ src/tk_producer.cpp
@@ -19,7 +19,8 @@ void IsoTrackProducer::WriteIsoTracks(nano_tree &nano, pico_tree &pico,
   // N.B. Objects that end up in the slimmedElecrtons or slimmedMuons collections are not stored 
   // as IsoTrack in Nano so we have to loop over all three collections
   for (int itk(0); itk < nano.nIsoTrack(); itk++) {
-    IsGoodTk(pico, nano.IsoTrack_pdgId()[itk],
+    if (nano.IsoTrack_isPFcand()[itk]==0 || nano.IsoTrack_isFromLostTrack()[itk]==1) continue;
+    IsGoodTk(pico, false /*isNanoElectron*/ , false /*isNanoMuon*/ , nano.IsoTrack_pdgId()[itk],
              nano.IsoTrack_pt()[itk], nano.IsoTrack_eta()[itk], nano.IsoTrack_phi()[itk], 
              nano.IsoTrack_miniPFRelIso_chg()[itk], nano.IsoTrack_pfRelIso03_chg()[itk],
              nano.IsoTrack_dxy()[itk], nano.IsoTrack_dz()[itk],
@@ -28,8 +29,9 @@ void IsoTrackProducer::WriteIsoTracks(nano_tree &nano, pico_tree &pico,
 
   // collect relevant Electrons
   for (int iel(0); iel < nano.nElectron(); iel++) {
+    if (nano.Electron_isPFcand()[iel]==0) continue;
     if (find(sig_el_nano_idx.begin(), sig_el_nano_idx.end(), iel)!=sig_el_nano_idx.end()) continue;
-    IsGoodTk(pico, nano.Electron_pdgId()[iel],
+    IsGoodTk(pico, true /*isNanoElectron*/ , false /*isNanoMuon*/ , nano.Electron_pdgId()[iel],
              nano.Electron_pt()[iel], nano.Electron_eta()[iel], nano.Electron_phi()[iel], 
              nano.Electron_miniPFRelIso_chg()[iel], nano.Electron_pfRelIso03_chg()[iel],
              nano.Electron_dxy()[iel], nano.Electron_dz()[iel],
@@ -38,8 +40,9 @@ void IsoTrackProducer::WriteIsoTracks(nano_tree &nano, pico_tree &pico,
   
   // collect relevant Muons
   for (int imu(0); imu < nano.nMuon(); imu++) {
+    if (nano.Muon_isPFcand()[imu]==0) continue;
     if (find(sig_mu_nano_idx.begin(), sig_mu_nano_idx.end(), imu)!=sig_mu_nano_idx.end()) continue;
-    IsGoodTk(pico, nano.Muon_pdgId()[imu],
+    IsGoodTk(pico, false /*isNanoElectron*/ , true /*isNanoMuon*/ , nano.Muon_pdgId()[imu],
              nano.Muon_pt()[imu], nano.Muon_eta()[imu], nano.Muon_phi()[imu], 
              nano.Muon_miniPFRelIso_chg()[imu], nano.Muon_pfRelIso03_chg()[imu],
              nano.Muon_dxy()[imu], nano.Muon_dz()[imu],
@@ -48,7 +51,7 @@ void IsoTrackProducer::WriteIsoTracks(nano_tree &nano, pico_tree &pico,
   return;
 }
 
-bool IsoTrackProducer::IsGoodTk(pico_tree &pico, int pdgid, float pt, float eta, float phi, 
+bool IsoTrackProducer::IsGoodTk(pico_tree &pico, bool isNanoElectron, bool isNanoMuon, int pdgid, float pt, float eta, float phi, 
                                 float miniso_chg, float reliso_chg, float dxy, float dz, float mt){
   
   // re-applying cuts used in NanoAOD so that they would also apply to "tracks" in the lepton collections
@@ -63,13 +66,21 @@ bool IsoTrackProducer::IsGoodTk(pico_tree &pico, int pdgid, float pt, float eta,
   if (pdgid!=11 && pdgid!=13 && pdgid!=211) return false; 
   
   if (pdgid==11 || pdgid==13) {
-    if (pt < 5) return false;
-    if (pt < 25 && !(reliso_chg*pt < 5 || reliso_chg < 0.2)) return false; //combination of our old cuts & Nano baseline
-    if (pt>= 25 && !(reliso_chg < 0.2)) return false; //combination of our old cuts & Nano baseline
+    if (pt < 5) {
+      return false;
+    } else if (pt < 25) { // fail both relative and absolute isolation! (N.B. in this pT range, absolute is always looser...)
+      if (reliso_chg >= 0.2 && reliso_chg*pt >= 5) return false; 
+    } else {
+      if (reliso_chg >= 0.2) return false;
+    }
   } else {
-    if (pt < 10) return false;
-    if (pt < 25 && !(reliso_chg*pt < 5 || reliso_chg < 0.1)) return false; //combination of our old cuts & Nano baseline
-    if (pt>= 25 && !(reliso_chg < 0.1)) return false; //combination of our old cuts & Nano baseline
+    if (pt < 10) {
+      return false;
+    } else if (pt < 25) {
+      if (reliso_chg >= 0.1 && reliso_chg*pt >= 5) return false;
+    } else {
+      if (reliso_chg >= 0.1) return false;
+    }
   }
 
   if (fabs(eta) > 2.5) return false; // not applied to all tracks in Nano
@@ -87,6 +98,8 @@ bool IsoTrackProducer::IsGoodTk(pico_tree &pico, int pdgid, float pt, float eta,
   pico.out_tk_dxy().push_back(dxy);
   pico.out_tk_dz().push_back(dz);
   pico.out_tk_mt().push_back(mt); 
+  pico.out_tk_nano_electron().push_back(isNanoElectron); 
+  pico.out_tk_nano_muon().push_back(isNanoMuon); 
 
   pico.out_ntk()++;
   return true;
