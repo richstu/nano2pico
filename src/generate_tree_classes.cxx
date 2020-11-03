ee3ca1212fe0e1e815eda80e93bbefb3392f5fc0
mcoshiro <oshiro@ucsb.edu>
2020-11-02 23:34:04 -0800

    raised photon limit

--- src/generate_tree_classes.cxx
+++ src/generate_tree_classes.cxx
@@ -87,7 +87,7 @@ int GetArrayLength(const std::string var_name){
   else if (var_type == "Muon") return 40;
   else if (var_type == "Tau") return 30;
   else if (var_type == "IsoTrack") return 30;
-  else if (var_type == "Photon") return 30;
+  else if (var_type == "Photon") return 200;
   else if (var_type == "Jet") return 200;
   else if (var_type == "SoftActivityJet") return 30;
   else if (var_type == "FatJet") return 30;
