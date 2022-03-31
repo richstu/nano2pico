2585e6eed3598d384b7602c2ec88130b6ce11f37
Jaebak Kim <jbkim@ucsb.edu>
2022-03-31 16:55:21 -0700

    Changed linked file to actuall file.

--- scripts/make_run_scripts.py
+++ scripts/make_run_scripts.py
@@ -1,1 +1,16 @@
-../../draw_pico/scripts/make_run_scripts.py
+#!/usr/bin/env python3
+import sys
+import os
+
+# source_target = run/path_to_file.exe
+source_target = sys.argv[1]
+
+out_string = '''#!/bin/sh
+RUN_KERNEL=$(uname -r | cut -d '-' -f1)
+kernel/$RUN_KERNEL/'''+source_target+''' "$@"
+'''
+
+with open(source_target, 'w') as source_script:
+  source_script.write(out_string)
+  #print('Made '+source_script_path)
+  os.chmod(source_target, 0o755)
