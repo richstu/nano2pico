eb195720c121213d466471e3e125e9e64e67998d
Joseph David Grassi jdg282@cornell.edu <jdg282@compute-0-5.local>
2023-08-24 08:44:46 -0700

    small indent error fixed

--- src/zgamma_producer.cpp
+++ src/zgamma_producer.cpp
@@ -194,106 +194,106 @@ void ZGammaVarProducer::WriteZGammaVars(nano_tree &nano, pico_tree &pico, vector
 
 
 
-    // Bitmap for zgamma cut flow
-    int baseBit = 0b0000000000000;//If 0, no selections applied.
+  // Bitmap for zgamma cut flow
+  int baseBit = 0b0000000000000;//If 0, no selections applied.
 
-    float minlead_epT = 25; //probably store these in zgamma_producer.hpp
-    float minsublead_epT = 15;
-    float minlead_mupT = 20;
-    float minsublead_mupT = 10;
+  float minlead_epT = 25; //probably store these in zgamma_producer.hpp
+  float minsublead_epT = 15;
+  float minlead_mupT = 20;
+  float minsublead_mupT = 10;
 
-    bool trigs_e = false;
-    bool trigs_ee = false;
-    bool trigs_mu = false;
-    bool trigs_mumu = false;
+  bool trigs_e = false;
+  bool trigs_ee = false;
+  bool trigs_mu = false;
+  bool trigs_mumu = false;
 
-    switch(year){
-      case 2016:
-        trigs_e = pico.out_HLT_Ele27_WPTight_Gsf();
-        trigs_ee = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ();
-        trigs_mu = pico.out_HLT_IsoMu24() || pico.out_HLT_IsoTkMu24();
-        trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ() || pico.out_HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL() || pico.out_HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ();
-        break;
-      case 2017:
-        trigs_e = pico.out_HLT_Ele35_WPTight_Gsf();
-        trigs_ee = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL(); //DZ version has efficiency loss!!
-        trigs_mu = pico.out_HLT_IsoMu27();
-        trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8(); 
-        break;
-      case 2018:
-        trigs_e = pico.out_HLT_Ele35_WPTight_Gsf();
-        trigs_ee = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL(); 
-        trigs_mu = pico.out_HLT_IsoMu27() || pico.out_HLT_IsoMu24();
-        trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL();
-        break;
-      case 2022:
-        trigs_e = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL();
-        trigs_ee = pico.HLT_Ele30_WPTight_Gsf() || pico.out_HLT_Ele32_WPTight_Gsf() || pico.out_HLT_Ele35_WPTight_Gsf();
-        trigs_mu = pico.out_HLT_IsoMu24() || pico.out_HLT_IsoMu27();
-        trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL();
-        break;
-      case 2023:
-        cout<< "No cutflow for triggers specified for 2023 in zgammaproducer.cpp";
-        break;
-      default:
-        cout << "No cutflow single or dilepton triggers specified for given year" << endl;
-        exit(1);
-    }
+  switch(year){
+    case 2016:
+      trigs_e = pico.out_HLT_Ele27_WPTight_Gsf();
+      trigs_ee = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ();
+      trigs_mu = pico.out_HLT_IsoMu24() || pico.out_HLT_IsoTkMu24();
+      trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ() || pico.out_HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL() || pico.out_HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ();
+      break;
+    case 2017:
+      trigs_e = pico.out_HLT_Ele35_WPTight_Gsf();
+      trigs_ee = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL(); //DZ version has efficiency loss!!
+      trigs_mu = pico.out_HLT_IsoMu27();
+      trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8(); 
+      break;
+    case 2018:
+      trigs_e = pico.out_HLT_Ele35_WPTight_Gsf();
+      trigs_ee = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL(); 
+      trigs_mu = pico.out_HLT_IsoMu27() || pico.out_HLT_IsoMu24();
+      trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL();
+      break;
+    case 2022:
+      trigs_e = pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || pico.out_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL();
+      trigs_ee = pico.HLT_Ele30_WPTight_Gsf() || pico.out_HLT_Ele32_WPTight_Gsf() || pico.out_HLT_Ele35_WPTight_Gsf();
+      trigs_mu = pico.out_HLT_IsoMu24() || pico.out_HLT_IsoMu27();
+      trigs_mumu = pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8() || pico.out_HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL() || pico.out_HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL();
+      break;
+    case 2023:
+      cout<< "No cutflow for triggers specified for 2023 in zgammaproducer.cpp";
+      break;
+    default:
+      cout << "No cutflow single or dilepton triggers specified for given year" << endl;
+      exit(1);
+  }
 
-    //Read from left. Bit 1 n_ll>=2, Bit 2 n_ee>=2, Bit 3 n_mumu>=2, Bit 4 trigs single lep, Bit 5 trigs dilep, Bit 6 leading lepton pT, Bit 7 subleading lepton pT
-    // Bit 8 nphoton>=1, Bit 9 photon_id80, Bit 10 m_ll cut, Bit 11 m_lly cut, Bit 12 15/110, Bit 13 is 1 if outside m_lly signal region
+  //Read from left. Bit 1 n_ll>=2, Bit 2 n_ee>=2, Bit 3 n_mumu>=2, Bit 4 trigs single lep, Bit 5 trigs dilep, Bit 6 leading lepton pT, Bit 7 subleading lepton pT
+  // Bit 8 nphoton>=1, Bit 9 photon_id80, Bit 10 m_ll cut, Bit 11 m_lly cut, Bit 12 15/110, Bit 13 is 1 if outside m_lly signal region
 
 
 
-    if (pico.out_nel()>=2 && pico.out_ll_lepid().at(0)==11){
-      baseBit += 0b1010000000000; 
-      if(trigs_e == true){
-        baseBit +=0b0001000000000; 
-      }
-      if(trigs_ee == true){
-        baseBit +=0b0000100000000;
-      }
-      if (pico.out_el_pt().at(0)>=minlead_epT){
-        baseBit += 0b0000010000000;
-      }
-      if(pico.out_el_pt().at(1)>=minsublead_epT){
-        baseBit+= 0b0000001000000;
-      }
+  if (pico.out_nel()>=2 && pico.out_ll_lepid().at(0)==11){
+    baseBit += 0b1010000000000; 
+    if(trigs_e == true){
+      baseBit +=0b0001000000000; 
     }
-    if (pico.out_nmu()>=2 && pico.out_ll_lepid().at(0)==13){
-      baseBit += 0b1100000000000;
-      if(trigs_mu ==true){
-        baseBit +=0b0001000000000;
-      }
-      if(trigs_mumu == true){
-        baseBit +=0b0000100000000;
-      }
-      if (pico.out_mu_pt().at(0)>=minlead_mupT){
-        baseBit += 0b0000010000000;
-      }
-      if(pico.out_mu_pt().at(1)>=minsublead_mupT){
-        baseBit+= 0b0000001000000;
-      }
+    if(trigs_ee == true){
+      baseBit +=0b0000100000000;
     }
-    if (pico.out_nphoton()>=1){
-      baseBit+= 0b0000000100000;
+    if (pico.out_el_pt().at(0)>=minlead_epT){
+      baseBit += 0b0000010000000;
     }
-    if (pico.out_photon_id80().at(pico.out_llphoton_iph().at(0))){
-      baseBit+= 0b0000000010000;
+    if(pico.out_el_pt().at(1)>=minsublead_epT){
+      baseBit+= 0b0000001000000;
     }
-    if (pico.out_ll_m().at(pico.out_llphoton_ill().at(0))>=80 && pico.out_ll_m().at(pico.out_llphoton_ill().at(0))<=100){
-      baseBit+= 0b0000000001000;
+  }
+  if (pico.out_nmu()>=2 && pico.out_ll_lepid().at(0)==13){
+    baseBit += 0b1100000000000;
+    if(trigs_mu ==true){
+      baseBit +=0b0001000000000;
     }
-    if (pico.out_llphoton_m().at(0)>=100 && pico.out_llphoton_m().at(0)<=180){
-      baseBit+= 0b0000000000100;
+    if(trigs_mumu == true){
+      baseBit +=0b0000100000000;
     }
-    if(pico.out_photon_pt().at(pico.out_llphoton_iph().at(0))/pico.out_llphoton_m().at(0) >=15/110){
-      baseBit+= 0b0000000000010;
+    if (pico.out_mu_pt().at(0)>=minlead_mupT){
+      baseBit += 0b0000010000000;
     }
-    if(pico.out_llphoton_m().at(0)<=122 || pico.out_llphoton_m().at(0)>=128){
-      baseBit+= 0b0000000000001;
+    if(pico.out_mu_pt().at(1)>=minsublead_mupT){
+      baseBit+= 0b0000001000000;
     }
-    pico.out_zg_cutBitMap() = baseBit;
+  }
+  if (pico.out_nphoton()>=1){
+    baseBit+= 0b0000000100000;
+  }
+  if (pico.out_photon_id80().at(pico.out_llphoton_iph().at(0))){
+    baseBit+= 0b0000000010000;
+  }
+  if (pico.out_ll_m().at(pico.out_llphoton_ill().at(0))>=80 && pico.out_ll_m().at(pico.out_llphoton_ill().at(0))<=100){
+    baseBit+= 0b0000000001000;
+  }
+  if (pico.out_llphoton_m().at(0)>=100 && pico.out_llphoton_m().at(0)<=180){
+    baseBit+= 0b0000000000100;
+  }
+  if(pico.out_photon_pt().at(pico.out_llphoton_iph().at(0))/pico.out_llphoton_m().at(0) >=15/110){
+    baseBit+= 0b0000000000010;
+  }
+  if(pico.out_llphoton_m().at(0)<=122 || pico.out_llphoton_m().at(0)>=128){
+    baseBit+= 0b0000000000001;
+  }
+  pico.out_zg_cutBitMap() = baseBit;
 //End Bitmap for zgamma cut flow
 
   return;
