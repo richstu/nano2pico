05fa1111b8ca06f6cc1b9468e65023e94cf66dce
Katherine Lai mlai546@ucsb.edu <mlai@cms11.physics.ucsb.edu>
2026-02-25 11:46:24 -0800

    added new skim category for 1 lepton anti b

--- scripts/skim_file.py
+++ scripts/skim_file.py
@@ -62,6 +62,8 @@ def get_cuts(skim_name):
 
   if(skim_name=='higqcd'):  cuts = '&&'.join([njet_or_fjet_cut, 'nvlep==0', 'ntk==0', 'low_dphi_met'])
 
+  if(skim_name=='higlep1T_antib'): cuts = '&&'.join([njet_or_fjet_cut, 'nlep==1', pass_1l_trig30])
+
   # Loosen up just enough to do systematics - to be updated when needed
   sys_nbcut = 'max(nbt,Max$(sys_nbt))>=2'
   sys_njcut = '(njet==4||sys_njet[0]==4||sys_njet[1]==4||sys_njet[2]==4||sys_njet[3]==4||njet==5||sys_njet[0]==5||sys_njet[1]==5||sys_njet[2]==5||sys_njet[3]==5)'
