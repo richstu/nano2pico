1e15c8bb7b3eded4872fa3612b7adfcf733c50e4
Joseph David Grassi jdg282@cornell.edu <jdg282@cms37.physics.ucsb.edu>
2026-03-16 10:38:47 -0700

    small bug fix and new golden jsons

--- src/mu_producer.cpp
+++ src/mu_producer.cpp
@@ -18,7 +18,8 @@ MuonProducer::MuonProducer(string year_, bool isData_, float nanoaod_version_, s
   rng(4357),
   nanoaod_version(nanoaod_version_),
   run3(false){
-  if (year=="2022") {
+  if(year == "2016" || year == "2017" || year == "2018");
+  else if (year=="2022") {
     cs_scare_ = correction::CorrectionSet::from_file(
         "data/zgamma/2022/muon_scalesmearing.json");
     run3 = true;
